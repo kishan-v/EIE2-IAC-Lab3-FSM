5573fea4bfa89c16849c99f12051300c0f4905b5
kishan-v <kishanvijayarajah03@gmail.com>
2022-11-04 19:15:44 +0000

    Updated with comments to understand module

--- task3/clktick.sv
+++ task3/clktick.sv
@@ -1,29 +1,27 @@
 module clktick #(
-	parameter WIDTH = 16
-)(
-  // interface signals
-  input  logic             clk,      // clock 
-  input  logic             rst,      // reset
-  input  logic             en,       // enable signal
-  input  logic [WIDTH-1:0] N,     	 // clock divided by N+1
-  output logic  		       tick      // tick output
+    parameter WIDTH = 16
+) (
+    // interface signals
+    input  logic             clk,  // clock 
+    input  logic             rst,  // reset
+    input  logic             en,   // enable signal
+    input  logic [WIDTH-1:0] N,    // clock divided by N+1
+    output logic             tick  // tick output
 );
 
-logic [WIDTH-1:0] count;
+  logic [WIDTH-1:0] count;
 
-always_ff @ (posedge clk)
-    if (rst) begin
-        tick <= 1'b0;
-        count <= N;  
-        end
-    else if (en) begin
-	    if (count == 0) begin
-		    tick <= 1'b1;
-		    count <= N;
-	        end
-	    else begin
-		    tick <= 1'b0;
-		    count <= count - 1'b1;
-	        end
-        end
+  always_ff @(posedge clk)
+    if (rst) begin  // Reset down-counter that counts from N to 0 back to N
+      tick  <= 1'b0;  // Set tick signal low
+      count <= N;
+    end else if (en) begin
+      if (count == 0) begin  // once down counter reaches zero..
+        tick  <= 1'b1;  //.. 'tick' by asserting high to initiate pulse
+        count <= N;  // reset its counter back to N
+      end else begin
+        tick  <= 1'b0;  // if still counting from N to 0 set tick low
+        count <= count - 1'b1;  // count down by one
+      end
+    end
 endmodule
